a7e292ae5a7ad92093b521d29222572475edef28
Henry T <ton.henry92@gmail.com>
2021-07-25 16:12:01 -0400

    init

---
+++ src/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Menu from 'devextreme-react/menu';
+import 'whatwg-fetch';
+
+const menuOptions = [{
+    id: 0,
+    name: 'Home'
+}, {
+    id: 1,
+    name: 'User Registration',
+    onClick: () => window.location = '/'
+}, {
+    id: 1,
+    name: 'Upload Enrollment Sheet',
+    onClick: () => window.location = '/enroll'
+}, {
+    id: 2,
+    name: "LISP Parser",
+    onClick: () => window.location = '/parse'
+}];
+
+function Header() {
+    return(
+        <React.Fragment>
+            <div className={'logo'}>
+                <img src={'/logo192.png'} width={'50'} height={50} alt={'Demo'}></img>
+            </div>
+            <Menu
+                dataSource={menuOptions}
+                displayExpr={'name'}
+                orientation={'horizontal'}
+            />
+        </React.Fragment>
+    );
+}
+
+export default Header;
